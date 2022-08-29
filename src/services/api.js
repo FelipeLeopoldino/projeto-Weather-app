a7d4a6ecd89136f18ac5d5a322fd9fc9e709cb81
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-08-29 09:35:52 -0300

    eslint

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,8 @@
 const KEY = '5546e54b4e9d404d911113309221704';
 const fetchData = async (city) => {
   const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
-
   const fetchResponse = await fetch(url);
   const data = await fetchResponse.json();
-  
   return data;
 };
-
 export default fetchData;
